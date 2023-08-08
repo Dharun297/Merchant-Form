f44990b0b097b5793affdf4b31e0187736dbdd4b
Dharun297 <dharunpnb@gmail.com>
2023-08-08 16:55:21 +0530

    changes

---
+++ form.js
@@ -0,0 +1,22 @@
+function process(){
+    var fname = document.getElementById("name").value;
+    var gmail = document.getElementById("mail").value;
+    var number = document.getElementById("mobilenumber").value;
+    var number = document.getElementById("website").value;
+    var number = document.getElementById("contactname").value;
+    var number = document.getElementById("contactnumber").value;
+    var number = document.getElementById("contact email").value;
+    var number = document.getElementById("notes").value;
+    var number = document.getElementById("business").value;
+    var number = document.getElementById("").value;
+    var number = document.getElementById("entrepreneur").value;
+    var number = document.getElementById("products").value;
+    var number = document.getElementById("critical").value;
+    var number = document.getElementById("nocritical").value;
+
+    
+
+
+
+
+}
